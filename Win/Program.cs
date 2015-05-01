f202700b022fb56147eb8f75bdaa26da7b21c764
ts <1@1.com>
2015-05-01 23:57:10 +0400

    my first commit

--- Win/Program.cs
+++ Win/Program.cs
@@ -14,6 +14,7 @@ namespace Win
         [STAThread]
         static void Main()
         {
+            // ---------------------------
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Menu());
